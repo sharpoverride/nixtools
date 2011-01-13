77ab66a6932a78a6e766fc747de7f25bb5f46752
drusellers <dru@drusellers.com>
2011-01-12 22:08:36 -0600

    adding some rabbit awesome

---
+++ rabbitmq-mgmt/getmgmt.sh
@@ -0,0 +1,8 @@
+cd /usr/local/Cellar/rabbitmq/2.1.1/lib/rabbitmq/erlang/lib/rabbitmq-2.1.1/plugins
+
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/mochiweb-2.2.0.ez
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/webmachine-2.2.0.ez
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/amqp_client-2.2.0.ez
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/rabbitmq-mochiweb-2.2.0.ez
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/rabbitmq-management-agent-2.2.0.ez
+curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/rabbitmq-management-2.2.0.ez
