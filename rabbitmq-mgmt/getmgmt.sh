7bc6c1826e4c37c25f27e22fa6924c737e91dae3
drusellers <dru@drusellers.com>
2011-05-24 17:57:50 -0500

    tweaking bits and bobs

--- rabbitmq-mgmt/getmgmt.sh
+++ rabbitmq-mgmt/getmgmt.sh
@@ -1,5 +1,3 @@
-cd /usr/local/Cellar/rabbitmq/2.1.1/lib/rabbitmq/erlang/lib/rabbitmq-2.1.1/plugins
-
 curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/mochiweb-2.2.0.ez
 curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/webmachine-2.2.0.ez
 curl -O http://www.rabbitmq.com/releases/plugins/v2.2.0/amqp_client-2.2.0.ez
